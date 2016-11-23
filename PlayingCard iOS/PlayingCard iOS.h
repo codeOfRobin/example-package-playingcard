ebceffd96f9afb8df9ef5ce6a1a5f838eec9f487
codeOfRobin <me@rmalhotra.com>
2016-11-23 14:08:24 +0530

    added iOS target

---
+++ PlayingCard iOS/PlayingCard iOS.h
@@ -0,0 +1,19 @@
+//
+//  PlayingCard iOS.h
+//  PlayingCard iOS
+//
+//  Created by Robin Malhotra on 23/11/16.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlayingCard iOS.
+FOUNDATION_EXPORT double PlayingCard_iOSVersionNumber;
+
+//! Project version string for PlayingCard iOS.
+FOUNDATION_EXPORT const unsigned char PlayingCard_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayingCard_iOS/PublicHeader.h>
+
+
